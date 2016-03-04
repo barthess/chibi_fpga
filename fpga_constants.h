e28ce2f6f1bf481d9e2e541f66d1a4cabc1742a0
barthess <barthess@yandex.ru>
2016-03-04 16:27:36 +0300

    autogenerated file moved under version control

---
+++ fpga_constants.h
@@ -0,0 +1,15 @@
+// WARNING! Automatically generated from ../../xilinx/fsmc2wb/rtl/mtrx_math_constants.vhd
+// Do not modify it manually!
+#define MATH_OP_MUL    0
+#define MATH_OP_SCALE  1
+#define MATH_OP_TRN    2
+#define MATH_OP_CPY    3
+#define MATH_OP_SET    4
+#define MATH_OP_DIA    5
+#define MATH_OP_ADD    6
+#define MATH_OP_SUB    7
+#define MATH_OP_DOT    8
+#define MATH_OP_LAST   MATH_OP_DOT
+#define CMD_BIT_B_TR    13
+#define CMD_BIT_RESVD   14
+#define CMD_BIT_DV      15
